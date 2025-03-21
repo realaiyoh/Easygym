e0e6c2699c64283513956121b01519f93430d262
Ali <halilovicali@proton.me>
2025-03-21 02:45:50 +0100

    Setup tailwind, shadcn, theme switcher, prettier config

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import '@/App.css';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { ModeToggle } from '@/components/ModeToggle';
 
 function App() {
   return (
-    <>
-      <p>Easygym</p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <div className="bg-background">
+        <ModeToggle />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
